e588ce22f04584c834999c336ca7431ff7218dbf
timothydereuse <timdereuse@gmail.com>
2020-06-11 22:15:13 -0400

    visualization code moved to own file

--- build_mei_file.py
+++ build_mei_file.py
@@ -4,6 +4,7 @@ import json
 import parse_classifier_table as pct
 from pymei import MeiDocument, MeiElement, MeiAttribute, documentToText, documentToFile
 from itertools import groupby
+from visualize_alignment import draw_mei_doc
 
 
 def add_flags_to_glyphs(glyphs):
@@ -493,85 +494,6 @@ def process(jsomr, syls, classifier, width_mult=1, verbose=True):
     return documentToText(meiDoc)
 
 
-def draw_neume_alignment(in_png, out_fname, pairs, text_size=60):
-    '''
-    given the pairs from neume_to_lyric_alignment, draws the result on the original page (given a
-    path to the .png)
-    '''
-    fnt = ImageFont.truetype('FreeMono.ttf', text_size)
-    im = Image.open(in_png).convert('RGB')
-    draw = ImageDraw.Draw(im)
-
-    last_text = None
-    for gs, tb in pairs:
-        if not tb:
-            continue
-
-        draw.rectangle(tb['ul'] + tb['lr'], outline='black')
-        # draw.text(tb['ul'], tb['syl'], font=fnt, fill='gray')
-        for g in gs:
-
-            if 'clef' in g['name'] or 'custos' in g['name']:
-                continue
-
-            bb = g['bounding_box']
-            pt1 = (bb['ulx'] + bb['ncols'] // 2, bb['uly'] + bb['nrows'] // 2)
-            pt2 = ((tb['ul'][0] + tb['lr'][0]) // 2, (tb['ul'][1] + tb['lr'][1]) // 2)
-
-            if pt1[1] > pt2[1]:
-                continue
-
-            draw.line((pt1, pt2), fill='black', width=5)
-    im.save(out_fname)
-
-
-def draw_mei_doc(in_png, out_fname, meiDoc, text_size=60):
-    '''
-    given an encoded mei_doc result, draws the result on the original page (given a
-    path to the .png)
-    '''
-
-    fnt = ImageFont.truetype('FreeMono.ttf', text_size)
-    im = Image.open(in_png).convert('RGB')
-    draw = ImageDraw.Draw(im)
-
-    all_syllables = meiDoc.getElementsByName('syllable')
-    surface = meiDoc.getElementsByName('surface')[0]
-
-    # build a dictionary linking each zone's ID to its element object for easy access
-    surf_dict = {}
-    for c in surface.getChildren():
-        surf_dict[c.id] = {}
-        for coord in c.attributes:
-            surf_dict[c.id][coord.name] = int(coord.value)
-
-    for syllable in all_syllables:
-        neumes = syllable.getChildrenByName('neume')
-
-        for n in neumes:
-            nc_ids = [x.getAttribute('facs').value for x in n.children]
-            zones = [surf_dict[x] for x in nc_ids]
-            ulx = min([z['ulx'] for z in zones])
-            uly = min([z['uly'] for z in zones])
-            lrx = max([z['lrx'] for z in zones])
-            lry = max([z['lry'] for z in zones])
-
-            draw.rectangle((ulx, uly, lrx, lry), outline='black')
-            neume_avg_x = (ulx + lrx) // 2
-            neume_avg_y = (uly + lry) // 2
-
-            syl_zone = surf_dict[syllable.getAttribute('facs').value]
-            syl_avg_x = (syl_zone['ulx'] + syl_zone['lrx']) // 2
-            syl_avg_y = (syl_zone['uly'] + syl_zone['lry']) // 2
-
-            # don't draw a line to a blank syllable
-            if not syl_avg_x or syl_avg_y:
-                continue
-            draw.line((neume_avg_x, neume_avg_y, syl_avg_x, syl_avg_y), fill='black', width=3)
-
-    im.save(out_fname)
-
-
 if __name__ == '__main__':
 
     import PIL
@@ -609,14 +531,9 @@ if __name__ == '__main__':
 
         glyphs = add_flags_to_glyphs(glyphs)
         pairs = neume_to_lyric_alignment(glyphs, syl_boxes, median_line_spacing)
-        # draw_neume_alignment(in_png, out_fname_png, pairs)
-<<<<<<< HEAD
-        meiDoc = build_mei(pairs, staves, classifier)
-        meiDoc = merge_nearby_neume_components(meiDoc, width_multiplier=1)
-        draw_mei_doc(in_png, out_fname_png, meiDoc)
-=======
         meiDoc = build_mei(pairs, staves, classifier, jsomr['page'])
         meiDoc = merge_nearby_neume_components(meiDoc)
->>>>>>> 2ec9ac8c8f55403f003f921d7e7335e1aba1b18b
+
+        draw_mei_doc(in_png, out_fname_png, meiDoc)
 
         documentToFile(meiDoc, out_fname)
