DDMAL/MEI_encoding

commit 9ef9975bdd922a7a5f0e7f02a67dd55562449c47

Include version in title statement of outputs
When issues are found in generated files, this should help determine
which version of MEI_encoding was used to generate it (and if that issue
might have been addressed in a newer version).

build_mei_file.py
@@ -5,6 +5,7 @@ import parse_classifier_table as pct
 from pymei import MeiDocument, MeiElement, MeiAttribute, documentToText, documentToFile
 from itertools import groupby
 from visualize_alignment import draw_mei_doc
+from MEI_encoding import __version__
 
 
 def add_flags_to_glyphs(glyphs):
@@ -129,7 +130,7 @@ def generate_base_document():
     fileDesc.addChild(titleSt)
     title = MeiElement('title')
     titleSt.addChild(title)
-    title.setValue('MEI Encoding Output')
+    title.setValue('MEI Encoding Output (%s)' % __version__)
     pubStmt = MeiElement('pubStmt')
     fileDesc.addChild(pubStmt)
 

commit 711917f2460e50ad3a49c87fad7cd951c9232643

Fix issue introduced in 9ef9975bdd922a7a5f0e7f02a67
`from MEI_encoding import __version__` is treated as a relative import
and looks for a module `rodan.jobs.MEI_encoding.MEI_encoding`, which
does not exist.

Changing to `from rodan.jobs.MEI_encoding import __version__` fixes this
and lets the job be imported into rodan.

build_mei_file.py
@@ -5,7 +5,7 @@ import parse_classifier_table as pct
 from pymei import MeiDocument, MeiElement, MeiAttribute, documentToText, documentToFile
 from itertools import groupby
 from visualize_alignment import draw_mei_doc
-from MEI_encoding import __version__
+from rodan.jobs.MEI_encoding import __version__
 
 
 def add_flags_to_glyphs(glyphs):