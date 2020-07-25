dae68762c874527d14a7f2882dcaeed46d95e015
timothydereuse <timdereuse@gmail.com>
2020-07-24 20:09:07 -0400

    added test for oblique element

--- tests/test_MEI_glyph_creation.py
+++ tests/test_MEI_glyph_creation.py
@@ -9,11 +9,9 @@ class TestMEIGlyphCreation(unittest.TestCase):
     punctum_xml = '<neume>  <nc/>  </neume>'
     podastus3_xml = '<neume>  <nc/>  <nc intm="2S"/> </neume>'
     clefc_xml = '<clef line="4" oct="None" pname="None" shape="C" />'
-    dummy_surface = MeiElement('surface')
+    oblique3_xml = '<neume> <nc ligated="true"/>  <nc intm="-3S" ligated="true"/> </neume>'
 
     punctum_glyph = {'bounding_box': {'lrx': 1010, 'lry': 1010, 'ulx': 1000, 'uly': 1000},
-     # 'clef': 'clef.c',
-     # 'clef_pos': '7',
      'name': 'neume.punctum',
      'note': 'g',
      'octave': '2',
@@ -29,7 +27,18 @@ class TestMEIGlyphCreation(unittest.TestCase):
      'strt_pos': '4',
      'system_begin': False}
 
-    def test_punctum_element(self):
+    oblique3_glyph = {'bounding_box': {'lrx': 2648, 'lry': 5164, 'ulx': 2503, 'uly': 5040},
+      'name': 'neume.oblique3',
+      'note': 'b',
+      'octave': '2',
+      'staff': '14',
+      'strt_pos': '8',
+      'system_begin': False}
+
+    def setUp(self):
+        self.dummy_surface = MeiElement('surface')
+
+    def test_punctum_primitive(self):
         xml = ET.fromstring(self.punctum_xml)
         el = bmf.create_primitive_element(xml, self.punctum_glyph, self.dummy_surface)
 
@@ -38,11 +47,12 @@ class TestMEIGlyphCreation(unittest.TestCase):
         self.assertEqual(el.getAttribute('pname').value, self.punctum_glyph['note'])
         self.assertEqual(el.getAttribute('oct').value, self.punctum_glyph['octave'])
 
+        # make sure there are no extraneous elements
         exp_atts = set(['pname', 'oct', 'facs'])
         res_atts = set([x.name for x in el.attributes])
         self.assertEqual(exp_atts, res_atts)
 
-    def test_clefc_element(self):
+    def test_clefc_primitive(self):
         xml = ET.fromstring(self.clefc_xml)
         el = bmf.create_primitive_element(xml, self.clefc_glyph, self.dummy_surface)
 
@@ -50,10 +60,20 @@ class TestMEIGlyphCreation(unittest.TestCase):
         self.assertEqual(type(el), MeiElement)
         self.assertEqual(el.getAttribute('line').value, self.clefc_glyph['strt_pos'])
 
+        # make sure there are no extraneous elements
         exp_atts = set(['line', 'shape', 'facs'])
         res_atts = set([x.name for x in el.attributes])
         self.assertEqual(exp_atts, res_atts)
 
+    def test_oblique3_neume(self):
+        dummy_classifier = {'neume.oblique3': ET.fromstring(self.oblique3_xml)}
+        el = bmf.glyph_to_element(dummy_classifier, self.oblique3_glyph, self.dummy_surface)
+
+        self.assertEqual(type(el), MeiElement)
+        self.assertEqual(el.getAttributes(), [])
+
+        clds = el.getChildren()
+        self.assertEqual(len(clds), 2)
 
 if __name__ == '__main__':
     unittest.main()
