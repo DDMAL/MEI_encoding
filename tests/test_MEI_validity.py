f0bebb69fce112d993fafff9d3d49e0fa745458a
timothydereuse <timdereuse@gmail.com>
2020-08-06 16:42:41 -0400

    added tests for corrupting pitch and syl outputs

--- tests/test_MEI_validity.py
+++ tests/test_MEI_validity.py
@@ -20,17 +20,48 @@ class TestMEIValidity(unittest.TestCase):
         with open(self.syls_path) as file:
             self.syls = json.loads(file.read())
         self.classifier = pct.fetch_table_from_csv(self.classifier_path)
+        with open(self.rng_path) as file:
+            relaxng_doc = etree.parse(file)
+        self.rng = etree.RelaxNG(relaxng_doc)
 
     def test_build_mei(self):
+        '''
+        Ensures that the MEI file generated validates agains the MEI RNG schema.
+        '''
+        mei_text = bmf.process(self.jsomr, self.syls, self.classifier, width_mult=1, verbose=False)
+        mei_xml = etree.parse(StringIO(mei_text))
+
+        self.assertTrue(self.rng.validate(mei_xml))
+
+    def test_mei_remove_glyphs(self):
+        '''
+        Ensures that removing most of the glyphs from the pitch-finding output (all punctums)
+        still results in a valid MEI file.
+        '''
+        new_g = [x for x in self.jsomr['glyphs'] if not ('punctum' in x['glyph']['name'])]
+        self.jsomr['glyphs'] = new_g
 
         mei_text = bmf.process(self.jsomr, self.syls, self.classifier, width_mult=1, verbose=False)
         mei_xml = etree.parse(StringIO(mei_text))
 
-        with open(self.rng_path) as file:
-            relaxng_doc = etree.parse(file)
-        rng = etree.RelaxNG(relaxng_doc)
+        self.assertTrue(self.rng.validate(mei_xml))
 
-        self.assertTrue(rng.validate(mei_xml))
+    def test_mei_remove_syls(self):
+        '''
+        Ensures that removing most of the syllables (every other one) from the text-alignment output
+        still results in a valid MEI file.
+        '''
+        new_s = [x for i, x in enumerate(self.syls['syl_boxes']) if not i % 2]
+        self.syls['syl_boxes'] = new_s
+
+        mei_text = bmf.process(self.jsomr, self.syls, self.classifier, width_mult=1, verbose=False)
+        mei_xml = etree.parse(StringIO(mei_text))
+
+        self.assertTrue(self.rng.validate(mei_xml))
+
+
+
+
 
 
 if __name__ == '__main__':
