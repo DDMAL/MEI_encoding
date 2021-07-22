7be3484fca12b27bc0230c3b97946afaaba8feeb
GabbyHalpin <gabriellehalpin@icloud.com>
2021-07-22 11:41:26 -0400

    changes the swap

--- build_mei_file.py
+++ build_mei_file.py
@@ -39,7 +39,7 @@ def add_flags_to_glyphs(glyphs):
         temp2 = glyphs[i+1]
         midpoint = (temp1['bounding_box']['lrx'] + temp1['bounding_box']['ulx']) / 2
         if temp2['bounding_box']['ulx'] < midpoint and (temp1['bounding_box']['uly'] > temp2['bounding_box']['uly']):
-            temp1, temp2 = glyphs[i+1], glyphs[i]
+            glyphs[i+1], glyphs[i] = temp1, temp2
 
     # add flag to every glyph denoting whether or not a line break should come immediately after
     for i in range(len(glyphs)):
